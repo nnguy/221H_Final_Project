c8d60794eba4eab539f81e855497c0da83c65748
nnguy <36558416+nnguy@users.noreply.github.com>
2022-12-12 19:06:08 +0000

    filled out recipe class and added advisor class that interacts with the user

--- ingredient.h
+++ ingredient.h
@@ -8,8 +8,18 @@ private:
   double price; 
   double discount; 
 
-public: 
-  Ingredient(FOOD_GROUP, double, double); 
+public:
+  Ingredient(){
+    f = meat; 
+    price = 0; 
+    discount = 0; 
+  }
+  Ingredient(FOOD_GROUP, double, double);
+  void setPrice(double); 
+  void calcDiscount(double, double); 
+  FOOD_GROUP getFoodGroup () {return f;} 
+  double getPrice() {return price;}
+  double getDiscount() {return discount;} 
 
 };
 #endif 
